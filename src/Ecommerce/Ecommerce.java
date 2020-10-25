32d2cd63c99fa7070c304d2ba8bfcb3d06a1bb70
Praveen N <dsanjay@gmail.com>
2020-10-24 19:06:17 -0700

    Added Login redirect to the main menu if you run the file. Also finished basis of GUI on ecommerce website

---
+++ src/Ecommerce/Ecommerce.java
@@ -0,0 +1,42 @@
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ecommerce {
+    private JButton ViewItems;
+    private JButton ViewInventory;
+    private JPanel estore;
+    private JLabel welcomeLabel;
+    private JLabel Checkout;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton ViewItems = new JButton("View Items");
+        JButton ViewInventory = new JButton("View Inventory");
+        JButton Checkout = new JButton("Proceed to Checkout");
+        JPanel estore = new JPanel();
+        JLabel welcome = new JLabel("Welcome to the ecommerce store!");
+        JLabel Image = new JLabel(new ImageIcon("486.png"));
+
+        frame.setSize(1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(estore);
+
+        estore.setLayout(null);
+
+        ViewItems.setBounds(225, 400, 200, 200);
+        ViewInventory.setBounds(500, 400, 200, 200);
+        Checkout.setBounds(388, 700, 200, 200);
+        welcome.setBounds(375, 0, 200, 200);
+        Image.setBounds(100, 100, 200, 200);
+
+        estore.add(ViewItems);
+        estore.add(ViewInventory);
+        estore.add(welcome);
+        estore.add(Checkout);
+
+
+    }
+}
