8613685ad83dc21e3050bdaab6f1344c609a8c59
aryan114 <amberkararyan@gmail.com>
2020-10-27 08:31:45 -0700

    Final Commit

--- src/Ecommerce/Ecommerce.java
+++ src/Ecommerce/Ecommerce.java
@@ -1,9 +1,22 @@
+package Ecommerce;
+
+import view_control.CalculatorUI;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
 
-public class Ecommerce {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Ecommerce extends Items {
     private JButton ViewItems;
     private JButton ViewInventory;
     private JPanel estore;
@@ -17,7 +30,11 @@ public class Ecommerce {
         JButton Checkout = new JButton("Proceed to Checkout");
         JPanel estore = new JPanel();
         JLabel welcome = new JLabel("Welcome to the ecommerce store!");
-        JLabel Image = new JLabel(new ImageIcon("486.png"));
+        //JLabel Image = new JLabel(new ImageIcon("486.png"));
+        JLabel Image = new JLabel("");
+        java.awt.Image image = new ImageIcon("486.png").getImage();
+        Image.setIcon(new ImageIcon(image));
+        Image.setBounds(377, 111, 128, 128);
 
         frame.setSize(1000, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,9 +51,13 @@ public class Ecommerce {
 
         estore.add(ViewItems);
         estore.add(ViewInventory);
+        ViewInventory.addActionListener(e -> {
+            Items items = new Items();
+            items.setVisible(true);
+        });
+        estore.add(ViewInventory);
         estore.add(welcome);
         estore.add(Checkout);
-
 
     }
 }
