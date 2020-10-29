6ba4aabe8f284614717ca175869124ea10ccc500
Praveen N <dsanjay@gmail.com>
2020-10-28 20:18:50 -0700

    started the 2nd part of ecommerce website

---
+++ src/Ecommerce/Items.java
@@ -0,0 +1,13 @@
+package Ecommerce;
+import javax.swing.*;
+
+public class Items {
+    private JButton AddToCartMask;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton AddToCartMask = new JButton("Masks: $100");
+        JPanel estore = new JPanel();
+        JLabel welcome = new JLabel("Choose Items to Check Out");
+    }
+}
