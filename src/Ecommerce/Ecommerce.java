36ea88a773e8dc245025b9b7b65ebe43a3543057
aryan114 <amberkararyan@gmail.com>
2020-11-02 20:57:42 -0800

    Main menu working.

--- src/Ecommerce/Ecommerce.java
+++ src/Ecommerce/Ecommerce.java
@@ -1,7 +1,10 @@
 
 package Ecommerce;
 
+import Pythagoras.Pythagoras;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,34 +15,52 @@ public class Ecommerce extends JFrame {
     private JLabel welcomeLabel;
     private JLabel Checkout;
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
+    public Ecommerce() {
+        // JFrame frame = new JFrame();
+
+        setSize(1000, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        //add(estore);
+
         JButton ViewItems = new JButton("View Items");
         JButton ViewInventory = new JButton("View Inventory");
         JButton Checkout = new JButton("Proceed to Checkout");
-        JPanel estore = new JPanel();
+        //JPanel estore = new JPanel();
+        getContentPane().add(ViewItems);
+        getContentPane().add(ViewInventory);
+        getContentPane().add(Checkout);
         JLabel welcome = new JLabel("Welcome to the ecommerce store!");
-        JLabel Image = new JLabel(new ImageIcon("486.png"));
-        frame.setSize(1000, 1000);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.add(estore);
+        getContentPane().add(welcome);
+       // JLabel Image = new JLabel(new ImageIcon("486.png"));
 
-        estore.setLayout(null);
+        //estore.setLayout(null);
 
         ViewItems.setBounds(225, 400, 200, 200);
         ViewInventory.setBounds(500, 400, 200, 200);
         Checkout.setBounds(380, 700, 200, 200);
         welcome.setBounds(375, 0, 200, 200);
-        Image.setBounds(100, 100, 200, 200);
-
+        //Image.setBounds(100, 100, 200, 200);
+/*
         estore.add(ViewItems);
         estore.add(ViewInventory);
         estore.add(welcome);
         estore.add(Checkout);
-        estore.add(Image);
+        //estore.add(Image);
 
+ */
 
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Ecommerce ecommerceMarket = new Ecommerce();
+                ecommerceMarket.setTitle("Ecommerce Market");
+                ecommerceMarket.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
 
