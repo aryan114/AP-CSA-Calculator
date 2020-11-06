edaae9897380899e898a9611ce9ef3cceb7b916c
Praveen N <dsanjay@gmail.com>
2020-11-05 20:13:56 -0800

    Added more items

--- src/Ecommerce/Items.java
+++ src/Ecommerce/Items.java
@@ -27,7 +27,7 @@ public class Items extends JFrame {
         TotalMoney.setHorizontalAlignment(SwingConstants.RIGHT);
         TotalMoney.setBounds(18, 6, 650, 67);
         getContentPane().add(TotalMoney);
-
+//_______________________________________________________________________
         JButton apples = new JButton("Apples: $1.99");
         apples.addMouseListener(new MouseAdapter() {
             @Override
@@ -44,10 +44,48 @@ public class Items extends JFrame {
         apples.setOpaque(true);
         apples.setForeground(Color.BLACK);
         apples.setBackground(Color.ORANGE);
-        //apples.addActionListener(e -> (apples.getText()));
         apples.setBounds(35, 86, 100, 40);
         getContentPane().add(apples);
+//___________________________________________________________________________
+        JButton masks = new JButton("Masks: $1.99");
+        masks.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                apples.setBackground(Color.ORANGE);
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                apples.setBackground(Color.ORANGE);
+            }
+        });
+
+        masks.setBorder(new MatteBorder(4, 4, 4, 4, Color.BLACK));
+        masks.setOpaque(true);
+        masks.setForeground(Color.BLACK);
+        masks.setBackground(Color.ORANGE);
+        masks.setBounds(35, 136, 100, 40);
+        getContentPane().add(masks);
+        //_______________________________________________________________________________
+        JButton Calculator = new JButton("Calculator: $10.00");
+        Calculator.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                apples.setBackground(Color.ORANGE);
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                apples.setBackground(Color.ORANGE);
+            }
+        });
+
+        Calculator.setBorder(new MatteBorder(4, 4, 4, 4, Color.BLACK));
+        Calculator.setOpaque(true);
+        Calculator.setForeground(Color.BLACK);
+        Calculator.setBackground(Color.ORANGE);
+        Calculator.setBounds(35, 186, 100, 40);
+        getContentPane().add(Calculator);
     }
+
 
 
     public static void main(String[] args) {
